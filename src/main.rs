2e52a28d6ea99b0c526dd840640ae5fa7ca7f5e0
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-31 19:30:36 +0900

    add init cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command = args[1].as_str();
+    match command {
+        "init" => {
+            init().unwrap();
+        }
+        _ => {}
+    };
+}
+
+fn init() -> std::io::Result<()> {
+    fs::create_dir_all(".rgit/objects")?;
+    Ok(())
 }
