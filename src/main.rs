d96d6ae45ae8952a2bf68ee50f51260635368700
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-08-02 00:02:51 +0900

    create index

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::os::unix::fs::MetadataExt;
+use std::path::Path;
+
+const DIR_ROOT: &'static str = ".rgit";
 
 const DIR_OBJECTS: &'static str = ".rgit/objects";
 
@@ -55,6 +59,71 @@ fn add(file_path: &String) -> std::io::Result<()> {
 
     let mut file = File::create(full_dir.to_string() + "/" + file_name)?;
     file.write_all(&compressed)?;
+
+    add_entry_to_index(file_path, hashed.to_vec())?;
+
+    Ok(())
+}
+
+fn add_entry_to_index(file_path: &String, sha1_obj: Vec<u8>) -> std::io::Result<()> {
+    let path = DIR_ROOT.to_string() + "/index";
+    let path = Path::new(&path);
+    if path.exists() {
+        let mut file = File::open(path)?;
+    } else {
+        let mut file = File::create(path)?;
+
+        let mut buf = vec![];
+        write!(buf, "{}", "DIRC")?;
+        let ver: u32 = 0x0002;
+        buf.write_all(&ver.to_be_bytes())?;
+
+        let entry_size: u32 = 1;
+        buf.write_all(&entry_size.to_be_bytes())?;
+
+        let meta = fs::metadata(file_path)?;
+        let ctime: u32 = meta.ctime() as u32;
+        let ctime_nsec: u32 = meta.ctime_nsec() as u32;
+        let mtime: u32 = meta.mtime() as u32;
+        let mtime_nsec: u32 = meta.mtime_nsec() as u32;
+        let dev: u32 = meta.dev() as u32;
+        let ino: u32 = meta.ino() as u32;
+        let mode: u32 = meta.mode();
+        let uid: u32 = meta.uid();
+        let gid: u32 = meta.gid();
+        let size: u32 = meta.size() as u32;
+
+        buf.write_all(&ctime.to_be_bytes())?;
+        buf.write_all(&ctime_nsec.to_be_bytes())?;
+        buf.write_all(&mtime.to_be_bytes())?;
+        buf.write_all(&mtime_nsec.to_be_bytes())?;
+        buf.write_all(&dev.to_be_bytes())?;
+        buf.write_all(&ino.to_be_bytes())?;
+        buf.write_all(&mode.to_be_bytes())?;
+        buf.write_all(&uid.to_be_bytes())?;
+        buf.write_all(&gid.to_be_bytes())?;
+        buf.write_all(&size.to_be_bytes())?;
+
+        buf.write_all(&sha1_obj)?;
+
+        let name = Path::new(file_path).file_name().unwrap();
+        let name_len: u16 = name.len() as u16;
+        buf.write_all(&name_len.to_be_bytes())?;
+        write!(buf, "{}", name.to_str().unwrap())?;
+
+        // null padding
+        let pad_len = buf.len() % 8;
+        for _ in 0..pad_len {
+            buf.push(0);
+        }
+
+        let mut hasher = Sha1::new();
+        hasher.update(&buf);
+        let hashed = hasher.finalize();
+        buf.write_all(&hashed)?;
+
+        file.write_all(&buf)?;
+    }
 
     Ok(())
 }
